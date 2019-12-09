ed13831805cdc446ea68c3552bdce5730a8321ea
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2019-12-09 12:40:25 +0200

    Update main.js
    merge ךףץןם to  פמנצכ  by swith

--- main.js
+++ main.js
@@ -13,23 +13,23 @@ const hebrewToNumber = (heb) => {
         case "ח": temp=8;  break;
         case "ט": temp=9;  break;
         case "י": temp= 10;  break;
-        case "כ": temp=20;  break;
+        case "כ":
+        case "ך": temp=20;  break;
         case "ל": temp=30;  break;
+        case "ם":
         case "מ": temp=40;  break;
-        case "נ": temp =50;  break;
+        case "נ": 
+        case "ן": temp =50;  break;
         case "ס": temp=60;  break;
         case "ע": temp=70;  break;
-        case "פ": temp=80;  break;
+        case "פ":
+        case "ף": temp=80;  break;
+        case "ץ":
         case "צ": temp=90;  break;
         case "ק": temp=100;  break;
         case "ר": temp=200;  break;
         case "ש": temp=300;  break;
         case "ת": temp=400;  break;
-        case "ך": temp=20;  break;
-        case "ם": temp=40;  break;
-        case "ן": temp =50;  break;
-        case "ץ": temp =90;  break;
-        case "ף": temp=80;  break;
         default: temp=0;
         }
     sum+=temp;
